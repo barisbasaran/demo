c1b296e033e91f19f10c7dc1b47065571f3c6f3b
barisbasaran <basaran.baris@gmail.com>
2025-01-09 12:20:28 +0300

    setup

--- demo-mvc/src/test/java/com/baris/demo/ClientControllerTest.java
+++ demo-mvc/src/test/java/com/baris/demo/ClientControllerTest.java
@@ -4,9 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -19,17 +19,26 @@ public class ClientControllerTest {
     private TestRestTemplate template;
 
     @Test
-    public void getClient() {
+    public void createAndGetClient() {
         var client = Client.builder().name("ali").email("ali@yahoo.com").build();
         template.postForEntity("/clients", client, Client.class);
 
-        ResponseEntity<Client> response = template.getForEntity("/clients/1", Client.class);
-        var clientCreated = response.getBody();
-
-        assertThat(response.getStatusCode()).isEqualTo(HttpStatusCode.valueOf(200));
+        var clientCreated = template.getForObject("/clients/1", Client.class);
         assertThat(clientCreated).isNotNull();
         assertThat(clientCreated.getId()).isEqualTo(1);
         assertThat(clientCreated.getName()).isEqualTo("ali");
         assertThat(clientCreated.getEmail()).isEqualTo("ali@yahoo.com");
+
+        template.delete("/clients/1");
+    }
+
+    @Test
+    public void getClients() {
+        template.postForEntity("/clients", Client.builder().name("ali").email("ali@yahoo.com").build(), Client.class);
+        template.postForEntity("/clients", Client.builder().name("veli").email("veli@yahoo.com").build(), Client.class);
+
+        var clients = template.getForObject("/clients", List.class);
+
+        assertThat(clients.size()).isEqualTo(2);
     }
 }
