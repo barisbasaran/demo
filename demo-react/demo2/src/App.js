7dbb42177a6f700a9614317719299a9ae4e4c09b
barisbasaran <basaran.baris@gmail.com>
2025-01-09 20:10:45 +0300

    setup

--- demo-react/demo2/src/App.js
+++ demo-react/demo2/src/App.js
@@ -1,14 +1,94 @@
+import {useState} from 'react';
 
-function MyButton() {
-  return (
-      <button>I'm a button</button>
-  );
+function MyButton({count, onClick}) {
+    return (
+        <>
+            <button onClick={onClick}>
+                Clicked {count} times
+            </button>
+            <br/>
+        </>
+    );
 }
-export default function MyApp() {
-  return (
-      <div>
-        <h1>Welcome to my app</h1>
-        <MyButton />
-      </div>
-  );
+
+function MyApp() {
+    const [count, setCount] = useState(0);
+
+    function handleClick() {
+        setCount(count + 1);
+    }
+    return (
+        <div>
+            <h1>Counters that update separately</h1>
+            <MyButton count={count} onClick={handleClick}/>
+            <MyButton count={count} onClick={handleClick}/>
+        </div>
+    );
+}
+
+const user = {
+    name: 'Hedy Lamarr',
+    imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+    imageSize: 90,
+};
+
+function Profile() {
+    return (
+        <>
+            <h1>{user.name}</h1>
+            <img
+                className="avatar"
+                src={user.imageUrl}
+                alt={'Photo of ' + user.name}
+                style={{
+                    width: user.imageSize,
+                    height: user.imageSize
+                }}
+            />
+        </>
+    );
+}
+
+const products = [
+    {title: 'Cabbage', isFruit: false, id: 1},
+    {title: 'Garlic', isFruit: false, id: 2},
+    {title: 'Apple', isFruit: true, id: 3},
+];
+
+function Items() {
+    const listItems = products.map(product =>
+        <li key={product.id}>
+            {product.title}
+        </li>
+    );
+    return (
+        <ul>{listItems}</ul>
+    );
+}
+
+function ShoppingList() {
+    const listItems = products.map(product =>
+        <li
+            key={product.id}
+            style={{
+                color: product.isFruit ? 'magenta' : 'darkgreen'
+            }}
+        >
+            {product.title}
+        </li>
+    );
+    return (
+        <ul>{listItems}</ul>
+    );
+}
+
+export default function All() {
+    return (
+        <div>
+            <MyApp/>
+            <Profile/>
+            <Items/>
+            <ShoppingList/>
+        </div>
+    );
 }
