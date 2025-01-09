b4d618ffbeda9488fa02a6b4f77a694887a2e4b2
barisbasaran <basaran.baris@gmail.com>
2025-01-09 11:57:23 +0300

    setup

---
+++ demo-mvc/src/test/java/com/baris/demo/DemoMvcApplicationTest.java
@@ -0,0 +1,15 @@
+package com.baris.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("unit")
+class DemoMvcApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
