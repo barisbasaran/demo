96502be364932c4a248883c0ff7a908dece24181
barisbasaran <basaran.baris@gmail.com>
2025-01-12 16:35:17 +0300

    setup

---
+++ demo-react/demo3/src/App.js
@@ -0,0 +1,15 @@
+function MyApp() {
+    return (
+        <div>
+            <h1>Counters that update separately</h1>
+        </div>
+    );
+}
+
+export default function All() {
+    return (
+        <div>
+            <MyApp/>
+        </div>
+    );
+}
