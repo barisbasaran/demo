44ecf7af0bff2e51706ff93e099b1306d3c70943
barisbasaran <basaran.baris@gmail.com>
2025-01-13 22:06:02 +0300

    setup

--- demo-react/demo4/src/App.js
+++ demo-react/demo4/src/App.js
@@ -49,23 +49,23 @@ function ClientDetail({client, setClientId}) {
     );
 }
 
+async function fetchClients() {
+    try {
+        const response = await fetch("http://localhost:8080/clients");
+        return await response.json();
+    } catch (error) {
+        console.error("Error fetching clients", error);
+        return [];
+    }
+}
+
 export default function App() {
     const [clients, setClients] = useState([]);
     const [clientId, setClientId] = useState(null);
 
-    async function fetchClients() {
-        try {
-            const response = await fetch("http://localhost:8080/clients");
-            return await response.json();
-        } catch (error) {
-            console.error("Error fetching clients", error);
-            return [];
-        }
-    }
     function handleClick(id) {
         setClientId(id);
     }
-
     useEffect(() => {
         fetchClients().then((clients) => {
             setClients(clients);
