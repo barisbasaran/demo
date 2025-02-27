29ae06b841bd916ae82f837ba40b10ad7a6b9903
barisbasaran <basaran.baris@gmail.com>
2025-02-27 15:58:35 +0300

    setup

---
+++ demo-react/demo5/src/App.js
@@ -0,0 +1,15 @@
+import {useState} from 'react';
+
+function Header() {
+    return (
+        <h1>Hello!</h1>
+    );
+}
+
+export default function All() {
+    return (
+        <div>
+            <Header/>
+        </div>
+    );
+}
